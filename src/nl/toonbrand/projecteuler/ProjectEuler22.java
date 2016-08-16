fb127c859e520e075cbaef0aab2ea6a35639e593
Toon Brand <toon.brand@finalist.nl>
2016-08-16 14:47:03 +0200

    Problem 30 solved

--- src/nl/toonbrand/projecteuler/ProjectEuler22.java
+++ src/nl/toonbrand/projecteuler/ProjectEuler22.java
@@ -1,7 +1,8 @@
 package nl.toonbrand.projecteuler;
 
-import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -12,23 +13,30 @@ public class ProjectEuler22 {
     public static void main(String[] args) {
         final long startTime = System.currentTimeMillis();
 
-        String[] names = readNamesFile("p022_names.txt");
-        System.out.println("There are " + names.length + " names in the file. \nThe total score of all names is " + calculateNameScore(names));
+        String fileUrl = "https://projecteuler.net/project/resources/p022_names.txt";
+        System.out.println("Getting txt file from " + fileUrl);
+        String[] names = readNamesFile(fileUrl);
+        System.out.println("\nThere are " + names.length + " names in the file. \nThe total score of all names is " + calculateNameScore(names));
 
         final long endTime = System.currentTimeMillis();
         System.out.println("\n[Execution time: " + (endTime - startTime) + " milliseconds]");
     }
 
-    public static String[] readNamesFile(String fileName){
-        File file = new File(fileName);
+    public static String[] readNamesFile(String fileUrl){
         String fileContent = "";
 
         try{
-            Scanner sc = new Scanner(file);
-            fileContent = sc.next().replace("\"", "");
+            URL url = new URL(fileUrl);
+            Scanner s = new Scanner(url.openStream());
+            fileContent = s.next().replace("\"", "");
         }
         catch (FileNotFoundException e){
-            e.printStackTrace();
+            System.out.println("File not found, make sure the url is working.");
+            System.exit(0);
+        }
+        catch (IOException e){
+            System.out.println("Something went wrong, make sure your internet is working.");
+            System.exit(0);
         }
 
         String[] fileArray = fileContent.split(",");
---
+++ src/nl/toonbrand/projecteuler/ProjectEuler30.java
@@ -0,0 +1,38 @@
+package nl.toonbrand.projecteuler;
+
+
+/**
+ * Created by toon on 8/15/16.
+ */
+public class ProjectEuler30 {
+    public static void main(String[] args) {
+        final long startTime = System.currentTimeMillis();
+
+        System.out.println("Searching for numbers that can be written as the sum of the fifth powers of their digits...\n");
+        System.out.println("\nThe sum of all these numbers is "+sumFifthDigitPowers(calculateMax()));
+
+        final long endTime = System.currentTimeMillis();
+        System.out.println("\n[Execution time: " + (endTime - startTime) + " milliseconds]");
+    }
+
+    public static int calculateMax(){
+        return (int)Math.pow(9,5)*6;
+    }
+
+    public static int sumFifthDigitPowers(int max){
+        int sum = 0;
+        for (int i=2;i<max;i++){
+            int j=i;
+            int numTot=0;
+            while (j > 0) {
+                numTot+=Math.pow(( j % 10),5);
+                j = j / 10;
+            }
+            if (numTot==i){
+                sum+=i;
+                System.out.println(i + " qualifies");
+            }
+        }
+        return(sum);
+    }
+}
